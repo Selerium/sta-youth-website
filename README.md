4971ab1d0d3afaae2710baec7a00609fb396c307
Selerium <johnadithya008@gmail.com>
2025-03-16 22:53:33 +0400

    creating initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Youth @ STA Website
+
+A simple landing page for the youth ministry at STA. Built with Vue, Tailwind.
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Nav from "./components/Nav.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center w-11/12 mx-auto">
+    <Nav></Nav>
+    <RouterView class="w-11/12" />
+  </div>
+</template>
+
+<style scoped></style>
